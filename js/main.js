354ee96e2ed5c3216ae678ba1fc6c0a4d836d082
Navruzbek2103 <navrozbek2306@gmail.com>
2022-07-28 23:37:44 +0500

    Editted front

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ var elBody = document.querySelector("body");
 function showDate(){
   var newDate = new Date()
   document.querySelector(".time").textContent = newDate.getHours() + " : " + newDate.getMinutes()
-  document.querySelector(".timeSec").textContent = newDate.getSeconds()
+
+  document.querySelector(".timeSec").textContent = " : " + newDate.getSeconds()
 
   document.querySelector(".date").textContent = newDate.getDate() + ". " + newDate.getMonth() + ". " + newDate.getFullYear()
 }
